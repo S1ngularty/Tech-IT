81748336d998914bcc06c7207c570e2a810a292a
tolits20 <jacalanangelito@gmail.com>
2024-11-27 09:21:43 +0800

    updated database

--- Administrator/techit.sql
+++ Administrator/techit.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2024 at 08:46 AM
+-- Generation Time: Nov 27, 2024 at 02:19 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ USE `techit`;
 CREATE TABLE `account` (
   `account_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `role` enum('admin','user') NOT NULL,
   `profile_img` varchar(255) NOT NULL,
@@ -43,11 +43,10 @@ CREATE TABLE `account` (
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`account_id`, `user_id`, `username`, `password`, `role`, `profile_img`, `account_status`) VALUES
-(10, 17, 'Arthur@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'user', '672e0509748a74.92074139.png', 'activate'),
-(12, 19, 'kim@example.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'user', '67345043bbf208.78801629.png', 'deactivate'),
-(21, 30, 'asher@example.com', '1d30ced9ec7b700e17e7cce7c6c0a6c2c46af29c', 'admin', '6742d58338ac56.89428640.jpg', 'activate'),
-(22, 31, 'ego@example.com', '8ee7bd92db2d4239c746a393ab1b3dc706c5a44f', 'user', '6742d849b60f43.50373053.jpg', 'activate');
+INSERT INTO `account` (`account_id`, `user_id`, `email`, `password`, `role`, `profile_img`, `account_status`) VALUES
+(31, 40, 'user@example.com', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'user', '6745d88633a194.27699598.png', 'activate'),
+(32, 41, 'admin@example.com', 'bfe54caa6d483cc3887dce9d1b8eb91408f1ea7a', 'admin', '6745daaa027830.79065526.png', 'activate'),
+(34, 45, 'levi@example.com', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'admin', '6745e139257de2.90392168.png', 'activate');
 
 -- --------------------------------------------------------
 
@@ -62,15 +61,6 @@ CREATE TABLE `cart` (
   `quantity` int(11) NOT NULL,
   `date_placed` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`cart_id`, `account_id`, `product_id`, `quantity`, `date_placed`) VALUES
-(32, 10, 4, 1, '2024-11-18 13:36:12'),
-(33, 10, 5, 1, '2024-11-18 13:36:14'),
-(34, 10, 7, 1, '2024-11-18 13:36:15');
 
 -- --------------------------------------------------------
 
@@ -88,9 +78,9 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`category_id`, `category_name`) VALUES
-(5, 'Gaming'),
-(10, 'Laptops'),
-(4, 'Uncategorized'),
+(10, 'Laptop'),
+(16, 'Mouse'),
+(14, 'Uncategorized'),
 (8, 'Upgrade');
 
 -- --------------------------------------------------------
@@ -114,19 +104,8 @@ CREATE TABLE `orderline` (
 --
 
 INSERT INTO `orderline` (`orderline_id`, `order_id`, `product_id`, `quantity`, `unit_price`, `total_price`, `created`) VALUES
-(5, 5, 4, 1, 9000, 0, '0000-00-00 00:00:00'),
-(6, 5, 5, 1, 5000, 0, '0000-00-00 00:00:00'),
-(7, 6, 4, 1, 9000, 0, '0000-00-00 00:00:00'),
-(8, 6, 5, 1, 5000, 0, '0000-00-00 00:00:00'),
-(9, 6, 7, 1, 2999, 0, '0000-00-00 00:00:00'),
-(10, 9, 5, 3, 5000, 0, '0000-00-00 00:00:00'),
-(11, 10, 4, 1, 9000, 0, '0000-00-00 00:00:00'),
-(12, 10, 5, 1, 5000, 0, '0000-00-00 00:00:00'),
-(13, 11, 8, 1, 25000, 0, '0000-00-00 00:00:00'),
-(14, 12, 4, 1, 9000, 0, '0000-00-00 00:00:00'),
-(15, 13, 4, 1, 9000, 0, '0000-00-00 00:00:00'),
-(16, 14, 5, 1, 5000, 0, '0000-00-00 00:00:00'),
-(17, 15, 5, 1, 5000, 0, '0000-00-00 00:00:00');
+(31, 29, 13, 1, 200, 200, '2024-11-26 22:44:08'),
+(33, 30, 12, 1, 1500, 1500, '2024-11-26 23:12:26');
 
 -- --------------------------------------------------------
 
@@ -147,17 +126,30 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`order_id`, `account_id`, `orderDate`, `total_amount`, `status`) VALUES
-(5, 10, '2024-11-12 11:34:12', 14000, 'shipped'),
-(6, 10, '2024-11-13 07:48:47', 16999, 'shipped'),
-(7, 10, '2024-11-13 07:49:03', 0, 'shipped'),
-(8, 10, '2024-11-13 08:02:50', 0, 'shipped'),
-(9, 12, '2024-11-13 15:08:45', 15000, 'shipped'),
-(10, 10, '2024-11-14 00:03:23', 14000, 'pending'),
-(11, 10, '2024-11-14 00:04:34', 25000, 'pending'),
-(12, 10, '2024-11-16 21:24:17', 9000, 'pending'),
-(13, 10, '2024-11-16 21:31:32', 9000, 'pending'),
-(14, 10, '2024-11-16 22:28:08', 5000, 'pending'),
-(15, 10, '2024-11-16 22:28:33', 5000, 'pending');
+(28, 31, '2024-11-26 22:27:40', 1000, 'shipped'),
+(29, 31, '2024-11-26 22:44:08', 6200, 'shipped'),
+(30, 31, '2024-11-26 23:12:26', 1500, 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `order_details_view`
+-- (See below for the actual view)
+--
+CREATE TABLE `order_details_view` (
+`order_id` int(11)
+,`account_id` int(11)
+,`orderDate` datetime
+,`total_amount` int(11)
+,`status` enum('pending','shipped')
+,`product_id` int(11)
+,`product_name` varchar(100)
+,`product_img` varchar(255)
+,`quantity` int(11)
+,`unit_price` int(11)
+,`total_price` int(11)
+,`created` datetime
+);
 
 -- --------------------------------------------------------
 
@@ -179,11 +171,9 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `product_name`, `price`, `product_description`, `product_img`, `date_added`) VALUES
-(4, 'GPU', 9000, 'A GPU, or graphics processing unit, is an electronic circuit that\'s designed to process graphics and perform mathematical calculations quickly', '67247997b817f0.51533430.jpg', '2024-11-01 09:55:33'),
-(5, 'CPU', 5000, ' It is the primary component of a computer that performs most of the processing and control functions', '672479b4d18d24.55399969.jpg', '2024-11-01 10:12:27'),
-(7, 'Monitor', 2999, 'A monitor is an electronic output device used to display information being entered and processed on a computer.', '6724ea4c031a79.94144893.jpg', '2024-11-01 22:48:44'),
-(8, 'Laptop AL15-5', 25000, ' a portable computer that is small enough to use on ones lap, has its main components (as keyboard and display screen) combined in one unit, and can run on battery power.', '67250a940fa6a1.62943538.jpg', '2024-11-02 01:06:28'),
-(9, 'SSD', 5900, 'Solid-state drives (SSDs) are the most common storage drives today. SSDs are smaller and faster than hard disk drives (HDDs). ', '67275ac95e08a1.70537664.jpg', '2024-11-03 19:13:13');
+(5, 'CPU', 400, ' It is the primary component of a computer that performs most of the processing and control functions', '672479b4d18d24.55399969.jpg', '2024-11-01 10:12:27'),
+(12, 'Acer Nitro 5', 1500, 'acer Nitro 5 Gaming Laptop, 9th Gen Intel Core i5-9300H, NVIDIA GeForce GTX 1650, 15.6\" Full HD IPS Display, WiFi 6, Waves MaxxAudio, Backlit Keyboard (32GB RAM/512GB PCIe SSD/1TB HDD)', '6745d97f8142e2.90072598.png', '2024-11-26 22:21:51'),
+(13, 'PRO X SUPERLIGHT 2 DEX', 200, 'PRO X SUPERLIGHT 2 DEX is a 60 g asymmetrical mouse featuring advanced HERO 2 sensor, robust LIGHTSPEED wireless, and LIGHTFORCE switches while delivering up to 95 hours of battery life.', '6745da267084a0.63666419.png', '2024-11-26 22:24:38');
 
 -- --------------------------------------------------------
 
@@ -201,11 +191,9 @@ CREATE TABLE `product_category` (
 --
 
 INSERT INTO `product_category` (`category_id`, `product_id`) VALUES
-(5, 4),
 (8, 5),
-(10, 8),
-(5, 7),
-(8, 9);
+(10, 12),
+(14, 13);
 
 -- --------------------------------------------------------
 
@@ -223,30 +211,6 @@ CREATE TABLE `review` (
   `update_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `review`
---
-
-INSERT INTO `review` (`review_id`, `account_id`, `product_id`, `comment`, `rating`, `create_at`, `update_at`) VALUES
-(1, 10, 4, '', 0, '2024-11-16 13:33:39', '2024-11-16 13:33:39'),
-(2, 10, 4, '', 1, '2024-11-16 13:41:05', '2024-11-16 13:41:05'),
-(3, 10, 4, '', 2, '2024-11-16 14:24:57', '2024-11-16 14:24:57'),
-(4, 10, 4, '', 5, '2024-11-16 14:25:25', '2024-11-16 14:25:25'),
-(5, 10, 4, 'On you feet, dad.....,,.', 4, '2024-11-16 14:26:25', '2024-11-16 14:26:25'),
-(6, 10, 4, '', 5, '2024-11-16 14:26:28', '2024-11-16 14:26:28'),
-(7, 10, 5, '', 0, '2024-11-16 14:29:00', '2024-11-16 14:29:00'),
-(8, 10, 5, '', 0, '2024-11-16 14:29:34', '2024-11-16 14:29:34'),
-(9, 10, 5, 'asdasd', 4, '2024-11-16 14:30:51', '2024-11-16 14:30:51'),
-(10, 10, 5, 'asdasda', 1, '2024-11-16 14:32:01', '2024-11-16 14:32:01'),
-(11, 10, 5, 'esfdvd', 1, '2024-11-16 14:32:46', '2024-11-16 14:32:46'),
-(12, 10, 5, 'esfdvd', 1, '2024-11-16 14:32:47', '2024-11-16 14:32:47'),
-(13, 10, 5, 'dasdaasd', 2, '2024-11-16 14:33:21', '2024-11-16 14:33:21'),
-(14, 10, 5, 'asdasd', 3, '2024-11-16 14:34:09', '2024-11-16 14:34:09'),
-(15, 10, 5, 'Lorem ipsum', 4, '2024-11-16 14:34:40', '2024-11-16 14:34:40'),
-(16, 10, 4, '', 0, '2024-11-17 14:26:35', '2024-11-17 14:26:35'),
-(17, 10, 4, '', 0, '2024-11-17 14:27:16', '2024-11-17 14:27:16'),
-(18, 10, 4, 'Bankai Minazuki', 1, '2024-11-17 14:27:49', '2024-11-17 14:27:49');
-
 -- --------------------------------------------------------
 
 --
@@ -263,11 +227,9 @@ CREATE TABLE `stocks` (
 --
 
 INSERT INTO `stocks` (`product_id`, `stock`) VALUES
-(4, 11),
 (5, 15),
-(7, 33),
-(8, 6),
-(9, 13);
+(12, 14),
+(13, 14);
 
 -- --------------------------------------------------------
 
@@ -289,10 +251,18 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `age`, `sex`, `contacts`) VALUES
-(17, 'Arthur', 'Leywin', 25, 'Male', '9121233214'),
-(19, 'Kim', 'Yebes', 19, 'Male', '98766765'),
-(30, 'Levi Asher', 'Penaverde', 19, 'Male', '09385736287'),
-(31, 'Ianzaee', 'Ego', 69, 'Female', '09123456789');
+(40, 'User', 'User', 20, 'Male', '09801851852'),
+(41, 'Admin', 'Administrator', 25, 'Female', '09141541478'),
+(45, 'Levi', 'Asher', 22, 'Male', '09252562569');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `order_details_view`
+--
+DROP TABLE IF EXISTS `order_details_view`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `order_details_view`  AS SELECT `o`.`order_id` AS `order_id`, `o`.`account_id` AS `account_id`, `o`.`orderDate` AS `orderDate`, `o`.`total_amount` AS `total_amount`, `o`.`status` AS `status`, `ol`.`product_id` AS `product_id`, `p`.`product_name` AS `product_name`, `p`.`product_img` AS `product_img`, `ol`.`quantity` AS `quantity`, `ol`.`unit_price` AS `unit_price`, `ol`.`total_price` AS `total_price`, `ol`.`created` AS `created` FROM ((`orders` `o` join `orderline` `ol` on(`o`.`order_id` = `ol`.`order_id`)) join `product` `p` on(`ol`.`product_id` = `p`.`product_id`)) ;
 
 --
 -- Indexes for dumped tables
@@ -303,7 +273,7 @@ INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `age`, `sex`, `contact
 --
 ALTER TABLE `account`
   ADD PRIMARY KEY (`account_id`),
-  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `username` (`email`),
   ADD KEY `user_fk` (`user_id`);
 
 --
@@ -380,49 +350,49 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `orderline`
 --
 ALTER TABLE `orderline`
-  MODIFY `orderline_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `orderline_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- Constraints for dumped tables
